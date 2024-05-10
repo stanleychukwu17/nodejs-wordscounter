a79b661a70eea747aaa113b18bd5d528e30f2085
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-05-10 10:50:48 +0100

    refactored the code and commented on it properly

--- App.ts
+++ App.ts
@@ -1,16 +1,13 @@
 const fs = require('fs');
-const readline = require('readline');
-const { performance: perf } = require('perf_hooks');
+const readline = require('readline'); // use the readline module of Node.js
+const { performance: perf } = require('perf_hooks'); // use the performance module of Node.js
 
 async function Count_the_words() {
     // Record the start time
     const startTime = perf.now();
-
-    const fs = require('fs');
-    const readline = require('readline');
-    
     const fileStream = fs.createReadStream('great-gatsby.txt');
 
+    // Read the file from this stream
     const result = await Read_the_file_from_this_stream(fileStream);
 
     // Calculate the elapsed time
@@ -22,30 +19,43 @@ async function Count_the_words() {
 }
 
 const Read_the_file_from_this_stream = async (fileStream: any) => {
+    // stores each word found in the file(no duplicates are store here)
+    // result: [in, my, younger, age, and, more ...]
     const allWords: string[] = [];
+
+    // stores the frequency of each word, like {in: 2, my: 3, come: 2, and: 2, more: 5 ...}
     const wordFrequency: any = {};
 
+    // stores each unique frequency, no duplicates frequency is stored here - we use it to group every word that is displayed in each frequency
+    // result: [5, 3, 2]; if $wordFrequency = {in: 2, my: 3, come: 2, and: 2, more: 5 ...}, so it takes each unique frequency
     const eachUniqueFrequency: number[] = [];
+
+    // using $eachUniqueFrequency to group each word that is displayed in each frequency
+    // result: {5: [in, my, come, and, more], 3: [my, come, and], 2: [in, come, and, more]};
     const finalResult: any = {};
 
+    // used a promise so i can track how long it took the application to run
     return new Promise((resolve, reject) => {
-
+        // starts the process of reading the file line by line
         const rl = readline.createInterface({
             input: fileStream,
             crlfDelay: Infinity
         });
-        
+
+        // loop through the file line by line: i.e as each line is returned from the $r1
         rl.on('line', (line: string) => {
-            const eachWord = line.split(' ');
+            const eachWord = line.split(' '); // stores each of the word from the line into an array
     
-            // loop through each word to clean it up
+            // loop through each word to clean it up, then add it to $allWords, then check for the frequency and add it to $wordFrequency
             for (let i = 0; i < eachWord.length; i++) {
                 const word = eachWord[i].replace(/[^a-zA-Z0-9]/g, '').trim().toLowerCase();
     
                 if (word.length > 0) {
                     if (allWords.includes(word)) {
+                        // existing word, so we only increase the frequency of the word
                         wordFrequency[word] += 1;
                     } else {
+                        // new word found, adds it to the list of words, i.e $allWords and also adds it to the frequency list
                         allWords.push(word)
                         wordFrequency[word] = 1;
                     }
@@ -55,7 +65,7 @@ const Read_the_file_from_this_stream = async (fileStream: any) => {
     
         
         rl.on('close', () => {
-            // loop through wordFrequency
+            // loop through wordFrequency and arrange the words into the final result
             for (const [word, frequency] of Object.entries(wordFrequency)) {
                 if ((frequency as number) in finalResult) {
                     finalResult[frequency as number].push(word)
@@ -65,10 +75,10 @@ const Read_the_file_from_this_stream = async (fileStream: any) => {
                 }
             }
     
-            // sort eachUniqueFrequency
+            // sort eachUniqueFrequency, from highest to lowest (i.e descending order)
             eachUniqueFrequency.sort((a, b) => b - a);
     
-            // loop through eachUniqueFrequency and print the words that appear that much
+            // loop through eachUniqueFrequency, use the value to display the words that are in that frequency
             eachUniqueFrequency.forEach((frequency: number) => {
                 finalResult[frequency].forEach((word: string) => {
                     console.log(`word: ${word} - ${frequency}`)
@@ -76,10 +86,9 @@ const Read_the_file_from_this_stream = async (fileStream: any) => {
             })
     
             resolve('File processed successfully.')
-            // console.log();
         });
-
     })
 }
 
+// calls the function to count the words
 Count_the_words();
